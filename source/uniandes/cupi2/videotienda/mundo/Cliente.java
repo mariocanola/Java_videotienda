7454d042c1ebb211ab4293695f189b2f59013285
mariocanola <macanola0@soy.sena.edu.co>
2025-06-10 10:38:22 -0500

    se implemento un nuevo atributo el cual almacena el valor de las copias alquiladas por el cliente y se agrego la documentacion de cada uno de los metodo

--- source/uniandes/cupi2/videotienda/mundo/Cliente.java
+++ source/uniandes/cupi2/videotienda/mundo/Cliente.java
@@ -1,4 +1,5 @@
 package uniandes.cupi2.videotienda.mundo;
+import java.util.ArrayList;
 
 /**
  * Representa un cliente de la videotienda.
@@ -33,6 +34,12 @@ public class Cliente {
      */
 	private int saldo;
 	
+
+    /**
+     * Lista de copias alquiladas por el cliente.
+     */
+	private ArrayList<Copia> copiasAlquiladas;
+	
 	//-----------------------------------------------------------------
     // Constructor
     //-----------------------------------------------------------------
@@ -46,61 +53,123 @@ public class Cliente {
 	@SuppressWarnings("unused")
 	public Cliente (String laCedula, String elNombre, String laDireccion)
 	{
-		String cedula = laCedula;
-		String nombre = elNombre;
-		String direccion = laDireccion;
+		this.cedula = laCedula;
+		this.nombre = elNombre;
+		this.direccion = laDireccion;
+		this.saldo = 0;
+	    this.copiasAlquiladas = new ArrayList<Copia>();	
 	}
 	
+
+    //-----------------------------------------------------------------
+    // M�todos
+    //-----------------------------------------------------------------
+	
+	/**
+	 * Retorna la Cedula del cliente 
+	 * @return Valor de la cedula
+	 */
 	public String darCedula()
 	{
 		return cedula;
 	}
 	
+	/**
+	 * Retorna el saldo actual del cliente
+	 * @return Valor de saldo
+	 */
 	public int darSaldo()
 	{
 		return saldo;
 	}
 	
+	/**
+	 * implementacion del método darNombre
+	 * @return Valor de Nombre
+	 */
 	public String darNombre()
 	{
 		return nombre;
 	}
 	
+	/**
+	 * implementacion del método darSaldo
+	 * @return Valor de la Dirección
+	 */
 	public String darDireccion() 
 	{
 		return direccion; 
 	}
 	
+	/**
+     * Alquila una copia de película al cliente. <br>
+     * <b>pre: </b> La copia no está alquilada. <br>
+     * <b>post: </b> La copia se agrega a la lista de copias alquiladas del cliente.
+     * @param copia Copia a alquilar. copia != null.
+     */
 	public void alquilarCopia(Copia copia)
 	{
 		
 	}
 	
+	/**
+     * Carga saldo a la cuenta del cliente. <br>
+     * <b>pre: </b> monto > 0. <br>
+     * <b>post: </b> El saldo del cliente se incrementa en el monto especificado.
+     * @param monto Cantidad a cargar al saldo. monto > 0.
+     */
 	public void cargarSaldo(int monto)
 	{
 		
 	}
 	
+	/**
+     * Descarga saldo de la cuenta del cliente. <br>
+     * <b>pre: </b> monto > 0 y monto <= saldo. <br>
+     * <b>post: </b> El saldo del cliente se reduce en el monto especificado.
+     * @param monto Cantidad a descargar del saldo. monto > 0 y monto <= saldo.
+     */
 	public void descargarSaldo(int monto)
 	{
 		
 	}
 	
+	 /**
+     * Retorna el número de copias alquiladas por el cliente.
+     * @return Número de copias alquiladas.
+     */
 	public int darNumeroAlquiladas()
 	{
 		
 	}
 	
+	/**
+     * Retorna la lista de copias alquiladas por el cliente.
+     * @return Lista de copias alquiladas.
+     */
 	public ArrayList darAlquiladas()
 	{
 		
 	}
 	
+	/**
+     * Busca una copia específica entre las películas alquiladas por el cliente.
+     * @param pelicula Título de la película a buscar. pelicula != null.
+     * @param codigo Código de la copia a buscar. codigo > 0.
+     * @return Copia encontrada o null si no existe.
+     */
 	public Copia buscarPeliculaAlquilada(String pelicula, int codigo)
 	{
 		
 	}
 	
+	/**
+     * Devuelve una copia alquilada por el cliente. <br>
+     * <b>pre: </b> La copia está alquilada por este cliente. <br>
+     * <b>post: </b> La copia se remueve de la lista de copias alquiladas.
+     * @param pelicula Título de la película a devolver. pelicula != null.
+     * @param codigo Código de la copia a devolver. codigo > 0.
+     */x	
 	public void devolverCopia(String pelicula, int codigo)
 	{
 		
