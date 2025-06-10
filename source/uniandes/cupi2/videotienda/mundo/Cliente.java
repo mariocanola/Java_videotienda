12e561c4cb52a5950afcef5f2053686400421921
mariocanola <macanola0@soy.sena.edu.co>
2025-06-10 10:09:57 -0500

    se definieron las clases que se realizaran en la clase Cliente

--- source/uniandes/cupi2/videotienda/mundo/Cliente.java
+++ source/uniandes/cupi2/videotienda/mundo/Cliente.java
@@ -1,5 +1,37 @@
 package uniandes.cupi2.videotienda.mundo;
 
+/**
+ * Representa un cliente de la videotienda.
+ * Cada cliente tiene nombre, cédula, dirección y un saldo.
+ */
 public class Cliente {
 
+	//-----------------------------------------------------------------
+    // Atributos
+    //-----------------------------------------------------------------
+	
+    /**
+     * Nombre del cliente.
+     */
+	private String cedula; 
+	
+
+    /**
+     * Cedula del cliente.
+     */
+	private String nombre;
+	
+
+    /**
+     * Dirección del cliente.
+     */
+	private String direccion;
+	
+
+    /**
+     * Saldo del cliente.
+     */
+	private int saldo;
+	
+	
 }
