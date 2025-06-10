1b410767c3a1432fbaf00ba94a4668b7b4de2146
mariocanola <macanola0@soy.sena.edu.co>
2025-06-10 10:12:18 -0500

    definición de las clases a realizar en la clase Cliente

--- source/uniandes/cupi2/videotienda/mundo/Cliente.java
+++ source/uniandes/cupi2/videotienda/mundo/Cliente.java
@@ -33,5 +33,76 @@ public class Cliente {
      */
 	private int saldo;
 	
+	//-----------------------------------------------------------------
+    // Constructor
+    //-----------------------------------------------------------------
+
+    /**
+     * Crea un nuevo cliente con los datos proporcionados.
+     * @param laCedula Cédula del cliente. laCedula != null.
+     * @param elNombre Nombre del cliente. elNombre != null.
+     * @param laDireccion Dirección del cliente. laDireccion != null.
+     */
+	@SuppressWarnings("unused")
+	public Cliente (String laCedula, String elNombre, String laDireccion)
+	{
+		String cedula = laCedula;
+		String nombre = elNombre;
+		String direccion = laDireccion;
+	}
 	
+	public String darCedula()
+	{
+		return cedula;
+	}
+	
+	public int darSaldo()
+	{
+		return saldo;
+	}
+	
+	public String darNombre()
+	{
+		return nombre;
+	}
+	
+	public String darDireccion() 
+	{
+		return direccion; 
+	}
+	
+	public void alquilarCopia(Copia copia)
+	{
+		
+	}
+	
+	public void cargarSaldo(int monto)
+	{
+		
+	}
+	
+	public void descargarSaldo(int monto)
+	{
+		
+	}
+	
+	public int darNumeroAlquiladas()
+	{
+		
+	}
+	
+	public ArrayList darAlquiladas()
+	{
+		
+	}
+	
+	public Copia buscarPeliculaAlquilada(String pelicula, int codigo)
+	{
+		
+	}
+	
+	public void devolverCopia(String pelicula, int codigo)
+	{
+		
+	}
 }
